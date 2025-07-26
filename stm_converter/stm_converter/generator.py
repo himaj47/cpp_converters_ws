c44c7261166a71299a48a258446ad5e6b41fd0a9
himaj47 <himajjoshi932@gmail.com>
2025-07-26 08:59:14 +0530

    refactor: ROSMsggenerator - Generator, xmlParser - Parse

---
+++ stm_converter/stm_converter/generator.py
@@ -0,0 +1,92 @@
+from jinja2 import Environment, FileSystemLoader
+
+from ament_index_python.packages import get_package_share_directory
+import ros2interface.api as interface
+
+from rosidl_adapter.parser import MessageSpecification, PRIMITIVE_TYPES
+import yaml
+import os 
+from stm_converter.utils import get_msg_fields
+
+class Generator: 
+    def __init__(self, structs_found, struct_name, header:str, namespace, msg: MessageSpecification, interface_name):
+        pathToTemplates = os.path.join(get_package_share_directory("stm_converter"), "resource/templates")
+        self.env_ = Environment(loader=FileSystemLoader(pathToTemplates))
+
+        self.structs_found_ = structs_found
+        self.header_name_ = header
+        self.ns = namespace
+        self.interface_name = interface_name
+
+        self.msg_filename_ = msg.msg_name 
+        self.msg_content_ = tuple()
+        self.interface_type_ = None
+        self.msg = msg
+        self.struct_name = struct_name
+
+
+    def gen_msgs(self, file: str):
+        # raise TypeError(f"gen_msgs() msg")
+        template = self.env_.get_template("message.txt")
+        
+        # filename = self.msg.msg_name + ".msg"
+        filename = file
+        self.msg_content_ = get_msg_fields(self.msg)
+
+        context = {"msg": self.msg_content_}
+
+        with open(filename, mode="w", encoding="utf-8") as output:
+                output.write(template.render(context))
+
+    def gen_type_adapter(self, file: str): # use struct name from xmlParser class
+        template = self.env_.get_template("type_adapter.txt")
+        context = {"header": self.header_name_, 
+                   "msg_file_name": self.msg.msg_name,
+                   "struct_name": self.struct_name,
+                   "msg": self.msg_content_,
+                   "namespace": self.ns,
+                   "interface_name": self.interface_name
+                   }
+        
+        # temp_file_name = f"{self.header_name_}_type_adapter.hpp"
+        filename = file
+        
+        with open(filename, mode="w", encoding="utf-8") as output:
+                output.write(template.render(context))
+
+
+    def gen_msg_description(self, file: str):
+        filename = file
+        content = {}
+
+        content["fields"] = {}
+        for field in self.msg.fields:
+            if field.type.type not in PRIMITIVE_TYPES:
+                pass
+            content["fields"].update({field.name: field.type.type})
+
+        # TODO update content path to .../share/<project_name>/msg/ if required
+        # content["path"] = os.path.abspath(relative_path)
+        content["path"] = os.getcwd()
+
+        with open(filename, mode="w", encoding="utf-8") as output:
+                output.write(yaml.dump(content))
+
+
+    def check_existance(self): # use find_content_pkg function from xml_parser
+        is_present = False
+        pkgs = list(interface.get_interface_packages().keys())
+
+        msg_interfaces = interface.get_message_interfaces(pkgs)   
+        for pkg, msgs in msg_interfaces.items():
+            if "msg/" + self.msg_filename_ in msgs:
+                is_present = True
+                self.interface_type_ = f"{pkg}/msg/{self.msg_filename_}"
+
+        if not is_present:
+            print(f"{self.msg_filename_}.msg does not exist")
+
+        return is_present
+
+
+                    
---
+++ stm_converter/stm_converter/parser.py
@@ -0,0 +1,127 @@
+from pygccxml import utils
+from pygccxml import declarations
+from pygccxml import parser
+
+from rosidl_adapter.parser import MessageSpecification, Type, Field
+
+from stm_converter.utils import generate_msg_name, process_non_primitives, get_msg_fields
+from stm_converter.utils import DECLARATION_PREFIX, VECTOR_TYPE_PREFIX, VECTOR_TYPE_SUFFIX
+
+class Parser:
+    def __init__(self, filename:str, pkg_name:str, namespace="", dependencies=None):
+        # Find out the c++ parser
+        generator_path, generator_name = utils.find_xml_generator()
+
+        # Configure the xml generator
+        xml_generator_config = parser.xml_generator_configuration_t(
+            xml_generator_path=generator_path,
+            xml_generator=generator_name,
+            cflags="-Wno-pragma-once-outside-header")
+        
+        self.filename = filename
+        self.namespace = namespace
+        self.pkg_name = pkg_name
+        self.deps = dependencies
+        self.ns = None
+        self.user_ns = ""
+        self.struct_name = ""
+
+        self.structs = {}
+
+        self.decls = parser.parse([filename], xml_generator_config)
+        self.global_namespace = declarations.get_global_namespace(self.decls)
+        self.namespaces = self.global_namespace.namespaces()
+
+        # if namespace provided by the user
+        if namespace:
+            self.ns = self.global_namespace.namespace(namespace)
+
+        else:
+            for ns in self.namespaces:
+                self.user_ns = ns.name
+
+            self.ns = self.global_namespace.namespace(self.user_ns)
+
+    def get_decls(self):
+        msg_name = ''
+        fields = []
+
+        for decl in self.ns.declarations:
+            # TODO: check for opaque types
+            # if isinstance(decl, declarations.opaque_type_t):
+            #     print("opaque")
+            if isinstance(decl, declarations.class_t):
+                if str(decl.name).startswith(DECLARATION_PREFIX): continue
+                temp = {decl.name: {}}
+                self.struct_name = decl.name
+
+                msg_name += generate_msg_name(decl.name)
+                context_pkg = None
+
+                for var in decl.variables():
+                    var_type = ""
+
+                    field_name = var.name
+                    field_type = ''
+                    msg_fields = None
+
+                    # var.decl_type might be any typedef or using type
+                    # We need the underlying canoncical type
+                    canonical_type = declarations.type_traits.remove_alias(var.decl_type)
+
+                    # TODO: use isinstance() instead of type()
+                    if type(canonical_type) == declarations.cpptypes.char_t:
+                        field_type = "char"
+
+                    elif type(canonical_type) == declarations.cpptypes.bool_t:
+                        field_type = "bool"
+
+                    elif type(canonical_type) == declarations.cpptypes.signed_char_t:
+                        field_type = "int8"
+
+                    elif type(canonical_type) == declarations.cpptypes.unsigned_char_t:
+                        field_type = "uint8"
+
+                    elif type(canonical_type) == declarations.cpptypes.short_int_t:
+                        field_type = "int16"
+
+                    elif type(canonical_type) == declarations.cpptypes.short_unsigned_int_t:
+                        field_type = "uint16"    
+
+                    elif type(canonical_type) == declarations.cpptypes.int_t:
+                        field_type = "int32"
+
+                    elif type(canonical_type) == declarations.cpptypes.unsigned_int_t:
+                        field_type = "uint32"
+
+                    elif type(canonical_type) == declarations.cpptypes.long_int_t:
+                        field_type = "int64"
+
+                    elif type(canonical_type) == declarations.cpptypes.long_unsigned_int_t:
+                        field_type = "uint64"
+                    
+                    elif type(canonical_type) == declarations.cpptypes.float_t:
+                        field_type = "float32"
+
+                    elif type(canonical_type) == declarations.cpptypes.double_t:
+                        field_type = "float64"
+
+                    elif str(canonical_type).startswith(VECTOR_TYPE_PREFIX):
+                        vector_type = str(canonical_type).strip(VECTOR_TYPE_PREFIX).strip(VECTOR_TYPE_SUFFIX)
+                        context_pkg, msg_fields, field_type = process_non_primitives(vector_type, True, self.pkg_name, deps=self.deps)
+
+                    else:
+                        print(f"weird type found - {var.decl_type} - canonical type - {canonical_type}")
+                        context_pkg, msg_fields, field_type = process_non_primitives(typename=str(var.decl_type), pkg_name=self.pkg_name, deps=self.deps)
+                    
+                    field_type = Type(field_type, context_package_name=context_pkg)
+                    field = Field(type_=field_type, name=field_name)
+                    field.msg_fields = msg_fields
+                    fields.append(field)
+
+                    temp[decl.name].update({var.name: var_type})
+
+                self.structs.update(temp)
+
+        msg = MessageSpecification(pkg_name=self.pkg_name, msg_name=msg_name, fields=fields, constants=[])
+        return self.structs, msg
