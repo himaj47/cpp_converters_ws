f66b048519da3c598295c158099713304661c1bb
himaj47 <himajjoshi932@gmail.com>
2025-07-26 08:50:11 +0530

    fix: artifacts generation location
    added new cli arguments

--- stm_converter/stm_converter/main.py
+++ stm_converter/stm_converter/main.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
-from stm_converter.xml_parser import xmlParser
-from stm_converter.ros_msg_generator import ROSMsgGenerator
+from stm_converter.parser import Parser
+from stm_converter.generator import Generator
 
 def main():
     desc = "stm_converter arguments"
@@ -10,6 +10,8 @@ def main():
     parser.add_argument("filename", help="header file")
     parser.add_argument("-ns", "--namespace", help="namespace")
     parser.add_argument("-o", "--out-description", help="message description YAML")
+    parser.add_argument("-m", "--out-msg", help="ros message")
+    parser.add_argument("-t", "--out-adapter", help="type adaption")
     parser.add_argument("-p", "--package", help="ROS package name")
     parser.add_argument("-d", "--deps", help="ROS package dependecies")
 
@@ -26,23 +28,31 @@ def main():
         namespace = args.namespace
 
     msg_description = args.out_description
+    msg_with_path = args.out_msg
+    type_adapter = args.out_adapter
     pkg_name = args.package
 
     if not pkg_name:
         raise ValueError("Package name (--package) is required and was not set!")
+    if not msg_description:
+        raise ValueError("msg description (--out-description) is required and was not set!")
+    if not msg_with_path:
+        raise ValueError("msg name (--out-msg) is required and was not set!")
+    if not type_adapter:
+        raise ValueError("type adapter name (--out-adapter) is required and was not set!")
 
 
-    xml_parser = xmlParser(str(args.filename), pkg_name, namespace, dependencies)
-    structs_found, msg = xml_parser.get_decls()
+    parser = Parser(str(args.filename), pkg_name, namespace, dependencies)
+    structs_found, msg = parser.get_decls()
 
-    msg_gen = ROSMsgGenerator(structs_found, xml_parser.struct_name, filename, xml_parser.user_ns, msg_description, msg=msg, interface_name=pkg_name)
+    msg_gen = Generator(structs_found, parser.struct_name, filename, parser.user_ns, msg=msg, interface_name=pkg_name)
     if msg_gen.check_existance():
         print(f"{msg_gen.msg_filename_}.msg exists\ntype: {msg_gen.interface_type_}")
 
     else:
-        msg_gen.gen_msgs()
-        msg_gen.gen_type_adapter()
-        msg_gen.gen_msg_description(relative_file_path)
+        msg_gen.gen_msgs(msg_with_path)
+        msg_gen.gen_type_adapter(type_adapter)
+        msg_gen.gen_msg_description(msg_description)
 
 if __name__ == "__main__":
     main()
