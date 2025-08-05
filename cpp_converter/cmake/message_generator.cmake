7af4e747a2e66cd49ed771f6bf382b57b23be3a6
himaj47 <himajjoshi932@gmail.com>
2025-08-05 11:52:16 +0530

    fix: msg files does not exist - rosidl_generate_interfaces

--- cpp_converter/cmake/message_generator.cmake
+++ cpp_converter/cmake/message_generator.cmake
@@ -1,4 +1,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/type_case_converter.cmake")
+find_package(rosidl_default_generators REQUIRED)
 
 function(convert_to_ros_msg TARGET_NAME FILE)
     set(generator_pkg stm_converter)
@@ -17,11 +18,17 @@ function(convert_to_ros_msg TARGET_NAME FILE)
     set(description_file "${basename}_desc.yaml")
     set(type_adapter_file "${basename}_type_adapter.hpp")
 
-    set(msg_description "${CMAKE_CURRENT_BINARY_DIR}/${basename}_desc.yaml")
-    set(type_adapter "${CMAKE_CURRENT_BINARY_DIR}/${basename}_type_adapter.hpp")
+    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/msg")
+    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/msg_descriptions")
+    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/type_adapters")
+
+    set(msg_description "${CMAKE_CURRENT_BINARY_DIR}/msg_descriptions/${basename}_desc.yaml")
+    set(type_adapter "${CMAKE_CURRENT_BINARY_DIR}/type_adapters/${basename}_type_adapter.hpp")
 
     snake_to_pascal("${basename}" out_string)
-    set(ros_msg "${CMAKE_CURRENT_BINARY_DIR}/${out_string}.msg")
+
+    set(ros_msg "${CMAKE_CURRENT_BINARY_DIR}/msg/${out_string}.msg")
+    set(formatted_msg "${CMAKE_CURRENT_BINARY_DIR}:msg/${out_string}.msg")
 
     execute_process(
         COMMAND ros2 pkg prefix ${generator_pkg}
@@ -31,31 +38,29 @@ function(convert_to_ros_msg TARGET_NAME FILE)
     set(msg_generator_path "${stm_converter_prefix}/lib/${generator_pkg}/${generator_pkg}")
 
     set(cmd ${msg_generator_path} "${FILE}" --out-description "${msg_description}" --out-msg "${ros_msg}" --out-adapter "${type_adapter}" --package "${PROJECT_NAME}")
+
     if(PKG_DEPENDENCIES)
         message(WARNING "Package Dependencies: ${PKG_DEPENDENCIES}")
         string(REPLACE ";" " " deps_str "${PKG_DEPENDENCIES}")
         list(APPEND cmd --deps "${deps_str}")
     endif()
 
+    set(msg_src "${CMAKE_CURRENT_SOURCE_DIR}/msg/${out_string}.msg")
 
-    add_custom_command(
-    OUTPUT ${msg_description} ${type_adapter} ${ros_msg}
-    COMMAND ${cmd}
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    DEPENDS ${FILE}
-    COMMENT "Generating message description from ${FILE}" 
+    execute_process(
+        COMMAND ${cmd}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    )
+
+    rosidl_generate_interfaces(${PROJECT_NAME}
+        msg/${out_string}.msg
+        DEPENDENCIES ${PKG_DEPENDENCIES}
     )
 
     install(
         FILES ${msg_description}
         DESTINATION share/${PROJECT_NAME}/msg_descriptions
     )
-
-    # TODO: update the destination
-    # install(
-    #     FILES ${headerFile}
-    #     DESTINATION share/${PROJECT_NAME}/include
-    # )
 
     install(
         FILES ${type_adapter}
@@ -66,8 +71,9 @@ function(convert_to_ros_msg TARGET_NAME FILE)
         FILES ${ros_msg}
         DESTINATION share/${PROJECT_NAME}/msg
     )
- 
+
     add_custom_target(${TARGET_NAME}
-        DEPENDS ${msg_description}
+        DEPENDS ${msg_description} ${type_adapter} ${ros_msg}
     )
+
 endfunction()
