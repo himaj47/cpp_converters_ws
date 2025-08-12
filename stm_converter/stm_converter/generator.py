a86dfa68c2a32954f90b1cc8bf76b498e6ee496c
himaj47 <himajjoshi932@gmail.com>
2025-08-12 11:00:23 +0530

    test: generation of multiple msgs from a single header file containing multiple structs

--- stm_converter/stm_converter/generator.py
+++ stm_converter/stm_converter/generator.py
@@ -6,7 +6,7 @@ import ros2interface.api as interface
 from rosidl_adapter.parser import MessageSpecification, PRIMITIVE_TYPES
 import yaml
 import os 
-from stm_converter.utils import get_msg_fields
+from stm_converter.utils import get_msg_fields, MESSAGE_FILE_EXTENSION
 
 class Generator: 
     def __init__(self, struct_name, header:str, namespace, msg: MessageSpecification, interface_name):
@@ -17,7 +17,7 @@ class Generator:
         self.ns = namespace
         self.interface_name = interface_name
 
-        self.msg_filename_ = msg.msg_name 
+        # self.msg_filename_ = msg.msg_name 
         self.msg_content_ = tuple()
         self.interface_type_ = None
         self.msg = msg
@@ -29,63 +29,71 @@ class Generator:
         template = self.env_.get_template("message.txt")
         
         # filename = self.msg.msg_name + ".msg"
-        filename = file
-        self.msg_content_ = get_msg_fields(self.msg)
+        # if len(self.msg) == 1:
+        #     filename = file
 
-        context = {"msg": self.msg_content_}
+        # raise NameError(f"msgs found = {self.msg[1].msg_name}")
 
-        with open(filename, mode="w", encoding="utf-8") as output:
-                output.write(template.render(context))
+        for msg in self.msg:
+            self.msg_content_ = get_msg_fields(msg)
 
-    def gen_type_adapter(self, file: str): # use struct name from xmlParser class
-        template = self.env_.get_template("type_adapter.txt")
-        context = {"header": self.header_name_, 
-                   "msg_file_name": self.msg.msg_name,
-                   "struct_name": self.struct_name,
-                   "msg": self.msg_content_,
-                   "namespace": self.ns,
-                   "interface_name": self.interface_name
-                   }
+            context = {"msg": self.msg_content_}
+
+            # with open(filename, mode="w", encoding="utf-8") as output:
+            with open(file + msg.msg_name + MESSAGE_FILE_EXTENSION, mode="w", encoding="utf-8") as output:
+                    output.write(template.render(context))
+
+
+    # def gen_type_adapter(self, file: str): # use struct name from xmlParser class
+    #     template = self.env_.get_template("type_adapter.txt")
+    #     context = {"header": self.header_name_, 
+    #                "msg_file_name": self.msg.msg_name,
+    #                "struct_name": self.struct_name,
+    #                "msg": self.msg_content_,
+    #                "namespace": self.ns,
+    #                "interface_name": self.interface_name
+    #                }
         
-        # temp_file_name = f"{self.header_name_}_type_adapter.hpp"
-        filename = file
+    #     # temp_file_name = f"{self.header_name_}_type_adapter.hpp"
+    #     filename = file
         
-        with open(filename, mode="w", encoding="utf-8") as output:
-                output.write(template.render(context))
+    #     with open(filename, mode="w", encoding="utf-8") as output:
+    #             output.write(template.render(context))
 
 
-    def gen_msg_description(self, file: str):
-        filename = file
-        content = {}
+    # def gen_msg_description(self, file: str):
+    #     filename = file
+    #     content = {}
 
-        content["fields"] = {}
-        for field in self.msg.fields:
-            if field.type.type not in PRIMITIVE_TYPES:
-                pass
-            content["fields"].update({field.name: field.type.type})
+    #     content["fields"] = {}
+    #     for field in self.msg.fields:
+    #         if field.type.type not in PRIMITIVE_TYPES:
+    #             pass
+    #         content["fields"].update({field.name: field.type.type})
 
-        # TODO update content path to .../share/<project_name>/msg/ if required
-        # content["path"] = os.path.abspath(relative_path)
-        content["path"] = os.getcwd()
+    #     # TODO update content path to .../share/<project_name>/msg/ if required
+    #     # content["path"] = os.path.abspath(relative_path)
+    #     content["path"] = os.getcwd()
 
-        with open(filename, mode="w", encoding="utf-8") as output:
-                output.write(yaml.dump(content))
+    #     with open(filename, mode="w", encoding="utf-8") as output:
+    #             output.write(yaml.dump(content))
 
 
     def check_existance(self): # use find_content_pkg function from xml_parser
         is_present = False
         pkgs = list(interface.get_interface_packages().keys())
 
-        msg_interfaces = interface.get_message_interfaces(pkgs)   
-        for pkg, msgs in msg_interfaces.items():
-            if "msg/" + self.msg_filename_ in msgs:
-                is_present = True
-                self.interface_type_ = f"{pkg}/msg/{self.msg_filename_}"
+        msg_interfaces = interface.get_message_interfaces(pkgs) 
 
-        if not is_present:
-            print(f"{self.msg_filename_}.msg does not exist")
+        for msg_filename_ in self.msg:  
+            for pkg, msgs in msg_interfaces.items():
+                if "msg/" + msg_filename_.msg_name in msgs:
+                    is_present |= True
+                    self.interface_type_ = f"{pkg}/msg/{msg_filename_.msg_name}"
+
+            if not is_present:
+                is_present |= False
+                print(f"{msg_filename_.msg_name}.msg does not exist")
 
         return is_present
 
-
-                    
